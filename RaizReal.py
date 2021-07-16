b4560cc2620714997887132714342c1c3a152e65
Ana-Beatiz-Fontoura <87340756+Ana-Beatiz-Fontoura@users.noreply.github.com>
2021-07-16 00:19:33 -0300

    Add files via upload

---
+++ RaizReal.py
@@ -0,0 +1,12 @@
+while True:
+    numero = float(input('Entre com um número: '))
+
+    raiz = numero**0.5
+
+    if numero < 0:
+       print('Entrada inválida!')
+
+
+    else:
+      print('A raíz real do número é:',raiz)
+
